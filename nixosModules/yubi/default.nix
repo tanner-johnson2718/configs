ca03bad601467ebf6e349b37ed2d5aebc06bdbbd
gamebox0 <tanner.johnson2718@gmail.com>
2025-01-01 14:21:52 -0800

    ..

--- nixosModules/yubi/default.nix
+++ nixosModules/yubi/default.nix
@@ -1,3 +1,25 @@
+# Yubi Key PAM OTP module.
+#
+# Note to get serial number run 'nix-shell -p yubico-pam -p yubikey-manager' followed
+# by ykman list.
+#
+# Next run the following to save your key:
+#  1) nix-shell -p pam_u2f
+#  2) mkdir -p ~/.config/Yubico
+#  3) pamu2fcfg > ~/.config/Yubico/u2f_keys
+#
+# Next configure the key to use OTP
+#  1) nix-shell -p yubico-pam -p yubikey-manager
+#  2) ykman otp chalresp --touch --generate 2
+#  3) ykpamcfg -2 -v
+#
+# TODO investigate a Yubi Key U2F module where only the first set of commands
+# are required and the only config you need is:
+#   security.pam.services = {
+#     login.u2fAuth = true;
+#     sudo.u2fAuth = true;
+#   };
+
 {config, lib, ... }: 
 let
   cfg = config.yubi;
