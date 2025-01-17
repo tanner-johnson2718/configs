0a9c9290f889c91e00cce18da9284ce4dacad950
gamebox0 <tanner.johnson2718@gmail.com>
2025-01-16 20:34:54 -0800

    moving locked System into gitea-server

--- nixosConfigurations/gitea-server-qemu0/default.nix
+++ nixosConfigurations/gitea-server-qemu0/default.nix
@@ -18,6 +18,7 @@ in
       memorySize = 1024;
       forwardPorts = [
 	 { from = "host"; host.port = 2222; guest.port = 22; }
+	 { from = "host"; host.port = 8080; guest.port = 80; }
       ];
       qemu.consoles = lib.mkForce [];
     };
@@ -27,14 +28,12 @@ in
     # script attached to this config that can be invoked with nix run .#<sys>
     system.build.runTarget = gitea-server-qemu0.config.system.build.vm;
 
-    lockedSystem.enable = true;
-    lockedSystem.sshLoginKey = gamebox0.config.commonSystem.sshKey; 
-    lockedSystem.hashedPassword = gamebox0.config.commonSystem.hashedPassword;
-
     gitea-server.enable = true;
     gitea-server.sshPort = 22;
-    gitea-server.httpPort = 3000;
+    gitea-server.httpPort = 80;
     gitea-server.domain = "localhost";
+    gitea-server.sshLoginKey = gamebox0.config.commonSystem.sshKey; 
+    gitea-server.hashedPassword = gamebox0.config.commonSystem.hashedPassword;
 
   };
   
--- nixosModules/gitea-server/default.nix
+++ nixosModules/gitea-server/default.nix
@@ -8,8 +8,11 @@ in
   options = {
     gitea-server.enable = lib.mkEnableOption "Enable the gitea server module";
     gitea-server.sshPort = lib.mkOption { type = lib.types.port; };
+    gitea-server.gitSshPort = lib.mkOption { type = lib.types.port; };
     gitea-server.httpPort = lib.mkOption { type = lib.types.port; };
     gitea-server.domain = lib.mkOption { type = lib.types.str; };
+    gitea-server.sshLoginKey = lib.mkOption { type = lib.types.str; };
+    gitea-server.hashedPassword = lib.mkOption { type = lib.types.str; };
   };
 
   config = lib.mkIf cfg.enable {
@@ -21,20 +24,75 @@ in
       dump = {
 	enable = true;
 	type = "tar.gz";
-	interval = "01:00";
+	interval = "weekly";
 	file = "gitea-bak.tar.gz";
       };
 
       settings = {
 	session.COOKIE_SECURE = true;
 	service.DISABLE_REGISTRATION = true;
-	server.SSH_PORT = cfg.sshPort;
+	server.SSH_PORT = cfg.gitSshPort;
 	server.PROTOCOL = "https";
 	server.HTTP_PORT = cfg.httpPort;
 	server.DOMAIN = cfg.domain;
 	log.level = "Info";
-      };
-      
+      };  
+    };
+
+    services.openssh =
+    {
+      startWhenNeeded = true;
+      settings.X11Forwarding = false;
+      settings.UsePAM = false;
+      settings.UseDns = false;
+      settings.StrictModes = true;
+      settings.PrintMotd = false;
+      settings.PermitRootLogin = "prohibit-password";
+      settings.PasswordAuthentication = false;
+      settings.LogLevel = "INFO";
+      settings.KbdInteractiveAuthentication = false;
+      settings.GatewayPorts = "no";
+      settings.AuthorizedPrincipalsFile = "none";
+      settings.AllowUsers = [ "root" ];
+      settings.AllowGroups = [ "root" ];
+      ports = [ cfg.sshPort ];
+      openFirewall = false;
+      enable = true;
+      banner = ''
+===============================================================================
+=                               GO AWAY                                       =
+===============================================================================
+      '';
+      authorizedKeysInHomedir = false;
+      allowSFTP = false;
+    };
+
+    users.users.root.hashedPassword = cfg.hashedPassword;
+    users.mutableUsers = false;
+    users.users."root".openssh.authorizedKeys.keys = [ cfg.sshLoginKey ];
+
+    networking.firewall = 
+    {
+      enable = true;
+      allowPing = false;
+      rejectPackets = false;
+      filterForward = false;
+      allowedUDPPorts = lib.mkForce [];
+      allowedTCPPorts = [ 
+	cfg.sshPort
+	cfg.gitSshPort
+	cfg.httpPort
+      ];
+      checkReversePath = "strict";
+      trustedInterfaces = lib.mkForce [];
+      logRefusedPackets = true;
+      logReversePathDrops = true;
+      allowedUDPPortRanges = lib.mkForce [];
+      allowedTCPPortRanges = lib.mkForce [];
+      logRefusedUnicastsOnly = false;
+      logRefusedConnections = true;
+      autoLoadConntrackHelpers = false;
+      connectionTrackingModules = lib.mkForce [];
     };
   };
 }
