0aa22396e89c32c6b6313e0ee78d2d8c2736e621
Tanner Johnson <tanner.johnson2718@gmail.com>
2026-06-02 20:28:42 -0700

    ..

--- bashrc.sh
+++ bashrc.sh
@@ -13,8 +13,6 @@ export GIT_PS1_SHOWUNTRACKEDFILES=true
 export PROMPT_COLOR='34'
 export PS1='\n\[\033[01;''${PROMPT_COLOR}m\]\W\[\033[01;32m\]$(__git_ps1 " (%s)") \[\033[00m\] '
 
-dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:ctrl_modifier']"
-
 alias l="ls -CF --color=auto";
 alias e="exit";
 alias la="alias";
@@ -24,4 +22,4 @@ alias lu="systemctl list-units";
 alias gs="git status";
 alias gd="git diff";
 alias gdc="git add ./* ; git commit -m \"..\" ; git push";
-alias nvim="/nix/store/0iajsr2dpkv0h14h7w6mp404blald2g5-neovim-0.11.5/bin/nvim"
+alias nvim="~/nvim/bin/nvim"
